9e634ca53a0f493a469361b079f08a704feee736
RicardoAlanVargasGam <ricardoalanvargasgamboa@gmail.com>
2019-06-12 05:55:02 -0500

    Commit 18 Lesson 155 Create Visit entity solved

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Visit.java
@@ -1,8 +1,24 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "visits")
+
 public class Visit extends BaseEntity {
+
+    @Column(name = "date")
+    private LocalDate date;
+
+    @Column(name = "description")
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
+    private Pet pet;
+
+
 
     public LocalDate getDate() {
         return date;
@@ -28,9 +44,6 @@ public class Visit extends BaseEntity {
         this.pet = pet;
     }
 
-    private LocalDate date;
-       private String description;
-       private Pet pet;
 
 
 }
